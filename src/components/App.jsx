31b31f3d141d658ac1ee792729058a5ab79db04a
griseldagomez <grinoe123@gmail.com>
2024-08-07 09:21:39 +0200

    components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,37 +1,18 @@
-// Fichero src/components/App.jsx
+import CharacterList from ".//CharacterList";
+import CharacterCard from ".//CharacterCard";
+import CharacterDetail from ".//CharacterDetail";
+import Filter from ".//Filter";
 
 import { Link, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <h2>Este título aparece siempre</h2>
-
-      <Routes>
-        <Route
-          path="/contacto"
-          element={
-            <h2>
-              Este título solo aparece cuando la usuaria entra en la página de
-              contacto
-            </h2>
-          }
-        />
-      </Routes>
-
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Ir al inicio</Link>
-          </li>
-          <li>
-          <Link to="/contacto" className="link" id="contact-link" target="_blank">
-  Ir a contacto
-</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <>
+      <Filter />
+      <CharacterList/>
+      <CharacterCard />
+      <CharacterDetail/>
+      </>
   );
 };
 
